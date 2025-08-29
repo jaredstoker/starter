ec506e172876b25d639f45524011cc68a1c3e19a
Jared Stoker <stokerfam@gmail.com>
2025-08-29 14:41:55 -0600

    5656

---
+++ infra/modules/ecs/main.tf
@@ -0,0 +1,36 @@
+variable "container_image" {
+  description = "ECR image URI with tag"
+  type        = string
+}
+
+resource "aws_ecs_task_definition" "this" {
+  family                   = "${var.service_name}-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.cpu
+  memory                   = var.memory
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = var.service_name
+      image     = var.container_image
+      essential = true
+      portMappings = [
+        {
+          containerPort = var.container_port
+          hostPort      = var.container_port
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        for k, v in var.environment :
+        {
+          name  = k
+          value = v
+        }
+      ]
+    }
+  ])
+}
